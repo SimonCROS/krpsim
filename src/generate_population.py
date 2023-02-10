5a8efdbbc24c239e3f1237cb8f20243fc99ca338
SimonCROS <scros@student.42lyon.fr>
2023-02-10 16:26:23 +0100

    Removed comment

--- src/generate_population.py
+++ src/generate_population.py
@@ -55,23 +55,3 @@ def generate_population(base: Chromosome, processes: list[Process], start: float
         population.append(chromosome)
 
     return population
-
-# def generate_population(base: Chromosome, processes: list[Process], start: float, args) -> list[Chromosome]:
-#     population: list[Chromosome] = []
-#     i = 0
-
-#     for _ in range(args.population):
-#         chromosome = copy.deepcopy(base)
-#         for _ in range(args.iterations):
-#             p = random.choice(processes)
-#             chromosome.try_do_process(p, insert_padding=True)
-
-#             if i % 1000 == 0:
-#                 delta = time.time() - start
-#                 if delta >= args.delay:
-#                     return population
-
-#             i += 1
-#         population.append(chromosome)
-
-#     return population
