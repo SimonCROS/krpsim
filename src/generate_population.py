59ae51d24d952146e8a2619f8b4c0a2e0b78cca5
SimonCROS <scros@student.42lyon.fr>
2023-02-10 14:41:10 +0100

    Format generator

--- src/generate_population.py
+++ src/generate_population.py
@@ -15,13 +15,16 @@ def is_doable(chromosome: Chromosome, process: Process) -> bool:
 
 def load_doable_processes(chromosome: Chromosome, processes: list[Process], memoization: dict[tuple[int]: list[Process]] = {}) -> bool:
     if memoization.get(chromosome.stock) is None:
-        memoization[chromosome.stock] = list(process for process in processes if is_doable(chromosome, process))
+        memoization[chromosome.stock] = list(
+            process for process in processes if is_doable(chromosome, process))
     return len(memoization[chromosome.stock]) > 0
+
 
 def __rewind(chromosome: Chromosome, memoization: dict[tuple[int]: list[Process]]) -> int:
     last_process = chromosome.undo_last_process()
     memoization[chromosome.stock].remove(last_process)
     return len(memoization[chromosome.stock])
+
 
 def generate_population(base: Chromosome, processes: list[Process], start: float, args) -> list[Chromosome]:
     memoization: dict[tuple: tuple] = {}
@@ -34,9 +37,10 @@ def generate_population(base: Chromosome, processes: list[Process], start: float
             if not load_doable_processes(chromosome, processes, memoization):
                 doable = __rewind(chromosome, memoization)
                 while not doable:
-                    del memoization[chromosome.stock]  
+                    del memoization[chromosome.stock]
                     doable = __rewind(chromosome, memoization)
-            chromosome.try_do_process(random.choice(memoization[chromosome.stock]))
+            chromosome.try_do_process(
+                random.choice(memoization[chromosome.stock]))
 
             if i % 1000 == 0:
                 delta = time.time() - start
